26bcec8ee21b858f6d26a405b69ed46491c66197
Ken Chen <kenchenpcpm@gmail.com>
2021-02-21 11:05:59 -0800

    clean up in JS

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,12 @@
-// Assignment Code
+
 const specialCharacters = "!@#$%^&*()";
 const generateButton = document.getElementById('generate');
 generateButton.addEventListener('click', writePassword);
 
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 function generatePassword() {
